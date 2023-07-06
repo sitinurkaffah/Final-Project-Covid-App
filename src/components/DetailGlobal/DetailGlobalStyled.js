6bb68598b6d22e89434de9fab5913146084f2261
sitinurkaffah <92791203+sitinurkaffah@users.noreply.github.com>
2023-07-06 18:40:57 +0700

    Update DetailGlobalStyled.js

--- src/components/DetailGlobal/DetailGlobalStyled.js
+++ src/components/DetailGlobal/DetailGlobalStyled.js
@@ -14,12 +14,12 @@ const StyledDetailGlobal = styled.div`
 
     h1 {
      text-align: center;
-     font-size: 35px;
+     font-size: 40px;
     }
 
     h3 {
      text-align: center;
-     font-size: 30px;
+     font-size: 35px;
     }
 
     :nth-child(1) .type {
