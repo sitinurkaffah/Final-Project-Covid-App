0cfe7545fdf1df12119726843d68ef00b709ac66
sitinurkaffah <92791203+sitinurkaffah@users.noreply.github.com>
2023-07-06 16:36:55 +0700

    Update IndoStyled.js

--- src/components/Indonesia/IndoStyled.js
+++ src/components/Indonesia/IndoStyled.js
@@ -8,7 +8,7 @@ const StyledIndo = styled.div`
     h2 {
         text-align: center;
         color: #06D6A0;
-        font-size: 50px;
+        font-size: 100px;
         font-weight: bold;
         margin-bottom: 20px;
     }
