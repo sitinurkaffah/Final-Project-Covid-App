2b07bb04e3841e8e705390e98c45de399aa1ff5f
sitinurkaffah <92791203+sitinurkaffah@users.noreply.github.com>
2023-07-06 13:23:15 +0700

    Delete src directory

--- src/utils/constants/endpoints.js
+++
@@ -1,8 +0,0 @@
-const BASE_URL = "https://covid-fe-2023.vercel.app";
-
-const ENDPOINTS = {
-    INDONESIA: `${BASE_URL}/api/indonesia.json`,
-    GLOBAL: `${BASE_URL}/api/global.json`,
-};
-
-export default ENDPOINTS;
