d57016f1a91258661ef6ec096c581c6538e732df
sitinurkaffah <92791203+sitinurkaffah@users.noreply.github.com>
2023-09-07 19:24:20 +0700

    Update HelloStyled.js

--- src/components/Hello/HelloStyled.js
+++ src/components/Hello/HelloStyled.js
@@ -4,79 +4,83 @@ import styled from "styled-components";
 const StyledHello = styled.div`
   margin: 1rem;
 
-section {
-  display: flex;
-  flex-direction: column;
-  text-align: center;
-}
+  section {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+  }
 
-.left {
-  margin-bottom: 1rem;
-}
+  .left {
+    margin-bottom: 1rem;
+  }
 
-h2 {
-  color: #06D6A0;
-  margin-bottom: 1rem;
-  font-size: 2.44rem;
-}
+  h2 {
+    font-family: 'Roboto', sans-serif; /* Menggunakan Roboto atau jenis font yang Anda inginkan */
+    color: #06D6A0;
+    margin-bottom: 1rem;
+    font-size: 2.44rem;
+  }
 
-h3 {
-  color: #118AB2;
-  margin-bottom: 1rem;
-  font-size: 1.59rem;
-}
+  h3 {
+    font-family: 'Roboto', sans-serif; /* Menggunakan Roboto atau jenis font yang Anda inginkan */
+    color: #118AB2;
+    margin-bottom: 1rem;
+    font-size: 1.59rem;
+  }
 
-p {
-  color: #64748b;
-  margin-bottom: 1rem;
-}
+  p {
+    font-family: 'Roboto', sans-serif; /* Menggunakan Roboto atau jenis font yang Anda inginkan */
+    color: #64748b;
+    margin-bottom: 1rem;
+  }
 
-.button {
-  padding: 1rem 2rem;
-  border: none;
-  border-radius: 10px;
-  background-color: #06D6A0;
-  color: #fff;
-  font-weight: bold;
-  cursor: pointer;
-}
+  .button {
+    font-family: 'Roboto', sans-serif; /* Menggunakan Roboto atau jenis font yang Anda inginkan */
+    padding: 1rem 2rem;
+    border: none;
+    border-radius: 10px;
+    background-color: #06D6A0;
+    color: #fff;
+    font-weight: bold;
+    cursor: pointer;
+  }
 
-img{
-  width: 100%; 
-  max-height: 400px; 
-  object-fit: cover; 
-  border-radius: 10px; 
-}
+  img {
+    width: 100%;
+    max-height: 400px;
+    object-fit: cover;
+    border-radius: 10px;
+  }
 
-/* Medium Screen */
-@media (min-width: 768px) {
-/*
-* Nothing TODO Here.
-* We dont change style hello.
-*/
-}
+  /* Medium Screen */
+  @media (min-width: 768px) {
+    /*
+    * Nothing TODO Here.
+    * We dont change style hello.
+    */
+  }
 
-/* Large Screen */
-@media (min-width: 992px) {
-  max-width: 1200px;
-  margin: 3rem auto;
+  /* Large Screen */
+  @media (min-width: 992px) {
+    max-width: 1200px;
+    margin: 3rem auto;
 
-section {
-  margin: 0 1rem;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  text-align: left;
-}
+    section {
+      margin: 0 1rem;
+      flex-direction: row;
+      justify-content: space-between;
+      align-items: center;
+      text-align: left;
+    }
 
-.left {
-  flex-basis: 40%;
-}
+    .left {
+      flex-basis: 40%;
+    }
 
-.right {
-  flex-basis: 60%;
-}
-}
+    .right {
+      flex-basis: 60%;
+    }
+  }
 `;
 
 export default StyledHello;
